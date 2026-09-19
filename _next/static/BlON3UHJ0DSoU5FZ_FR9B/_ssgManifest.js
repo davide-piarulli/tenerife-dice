5ef75d9d78f8eabbfa672d08b9bc5d2864ea61d9
davide-piarulli <davide.piarulli.1@gmail.com>
2026-09-19 09:40:11 +0000

    Deploying to hostinger-deploy from @ davide-piarulli/tenerife-dice@fdc82c4ecccf51433032dcbc2ed8ad754f40a143 🚀

---
+++ _next/static/BlON3UHJ0DSoU5FZ_FR9B/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcategoria\u002F[category]","\u002Fcategoria\u002F[category]\u002Fpagina\u002F[page]","\u002Fnoticias\u002F[slug]","\u002Fnoticias\u002Fpagina\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
